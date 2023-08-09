1cdfbc847c35ee002b1b664756f145815ec73eb0
yixin-zhu <zhu_yixin@foxmail.com>
2023-08-09 17:02:06 +0800

    xeval test

--- code/evaluator_series/eval.py
+++ code/evaluator_series/eval.py
@@ -2,16 +2,15 @@ import os
 import argparse
 import pandas as pd
 import torch
+import json
 from evaluators.moss import Moss_Evaluator
 from evaluators.chatglm import ChatGLM_Evaluator
 from evaluators.minimax import MiniMax_Evaluator
-
 
 import time
 choices = ["A", "B", "C", "D"]
 
 def main(args):
-    # args are parsed from command line
     if "moss" in args.model_name:
         evaluator=Moss_Evaluator(
             choices=choices,
@@ -19,7 +18,6 @@ def main(args):
             model_name=args.model_name
         )
     elif "chatglm" in args.model_name:
-        # cuda_device specifies the GPU to use, only for chatglm
         if args.cuda_device:
             os.environ["CUDA_VISIBLE_DEVICES"] = args.cuda_device
         device = torch.device("cuda")
@@ -29,27 +27,57 @@ def main(args):
             model_name=args.model_name,
             device=device
         )
+    elif "minimax" in args.model_name:
+        evaluator=MiniMax_Evaluator(
+            choices=choices,
+            k=args.ntrain,
+            group_id=args.minimax_group_id,
+            api_key=args.minimax_key,
+            model_name=args.model_name
+        )
     else:
         print("Unknown model name")
         return -1
 
-    # subject is the topic to test
     subject_name=args.subject
-
-    # prepare the directory to save the result logs
     if not os.path.exists(r"logs"):
         os.mkdir(r"logs")
     run_date=time.strftime('%Y-%m-%d_%H-%M-%S',time.localtime(time.time()))
     save_result_dir=os.path.join(r"logs",f"{args.model_name}_{run_date}")
     os.mkdir(save_result_dir)
     print(subject_name)
+    
+    var_file_path=os.path.join('tasks',f'{subject_name}', 'xiezhi.v1.json')
+    jsonfile = open(var_file_path, encoding='utf-8')
 
-    # load the test data
+    # 初始化DataFrame
+    var_df = pd.DataFrame(columns=['id', 'question', 'A', 'B', 'C', 'D', 'answer', 'explanation'])
+
+    # 解析JSON数据，逐行添加到DataFrame中
+    idx = 0
+    for jsonline in jsonfile:
+        jsonline = json.loads(jsonline)
+        options = jsonline['options'].split('\n')
+        for o in options:
+            o = str(o).strip().strip(' "\'').strip('"').rstrip('"')
+        print(len(options))
+        var_df.loc[idx] = [
+            idx,  # id自动递增编号
+            jsonline['question'],
+            options[0],
+            options[1],
+            options[2],
+            options[3],
+            get_option(jsonline['answer'].strip().strip(' "\'').strip('"').rstrip('"'), options),  # 将答案转换为选项号，并存储在answer列中
+            ' '  # 将标签以逗号分隔，并存储在explanation列中
+        ]
+        idx += 1
+    correct_ratio = evaluator.eval_subject(subject_name, var_df, few_shot=args.few_shot,save_result_dir=save_result_dir)
+    print("Acc:",correct_ratio)
+
+    '''
     val_file_path=os.path.join('data/val',f'{subject_name}_val.csv')
     val_df=pd.read_csv(val_file_path)
-
-    # evaluate the test data
-    # if few_shot is True, then the model is trained on the dev data
     if args.few_shot:
         dev_file_path=os.path.join('data/dev',f'{subject_name}_dev.csv')
         dev_df=pd.read_csv(dev_file_path)
@@ -57,15 +85,27 @@ def main(args):
     else:
         correct_ratio = evaluator.eval_subject(subject_name, val_df, few_shot=args.few_shot,save_result_dir=save_result_dir)
     print("Acc:",correct_ratio)
+    '''
 
+def get_option(s, options):
+    if s == options[0]
+        return 'A'
+    elif s == options[1]
+        return 'B'
+    elif s == options[2]
+        return 'C'
+    elif s == options[3]
+        return 'D'
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ntrain", "-k", type=int, default=5)
+    parser.add_argument("--minimax_group_id", type=str,default="xxx")
+    parser.add_argument("--minimax_key", type=str,default="xxx")
     parser.add_argument("--few_shot", action="store_true")
     parser.add_argument("--model_name",type=str)
     parser.add_argument("--cot",action="store_true")
     parser.add_argument("--subject","-s",type=str,default="operating_system")
-    parser.add_argument("--cuda_device", type=str)    
+    parser.add_argument("--cuda_device", type=str)
     args = parser.parse_args()
     main(args)
